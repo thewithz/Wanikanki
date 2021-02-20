822a4b87de305827aaf3e207e64d79886401b5c3
Nathan Withers <nathan@thewithz.tech>
2021-02-20 14:17:03 -0500

    Adds some library functions and a default config

---
+++ wanikani_api.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# a library of functions for interacting with wanikani's api
+
+API_ROOT="https://api.wanikani.com/v2"
+
+HEADERS=(
+    -H "\"Wanikani-Revision: $(jq -r '.api_version' config.json)\""
+    -H "\"Authorization: Bearer $(jq -r '.api_key' config.json)\""
+)
+
+wanikani::subjects() {
+    echo "$API_ROOT/subjects?"
+}
+
+wanikani::assignments() {
+    echo "$API_ROOT/assignments?"
+}
+
+# Return subjects of the specified types.
+# $@: radical | kanji | vocabulary
+types() {
+    (( "$#" >= 1 && "$#" <= 3)) ||
+        error "Invalid number of parameters. (given $#, expected [1,3])"
+    for arg in "$@"
+    do
+        case "$arg" in
+            radical | kanji | vocabulary)
+                ;;
+            *)
+                error "Invalid parameter. (given $arg, expected [radical | kanji | vocabulary])"
+                ;;
+        esac
+    done
+    # replace spaces with commas
+    values=$(tr -s '[:blank:]' ',' <<< "$*")
+    echo "$(cat -)&${FUNCNAME[0]}=$values"
+}
+
+# Only objects where the associated subject level matches one of
+# the array values are returned. Valid values range from 1 to 60.
+# $@: arbitrary length array of integers. Max length is 60 (there's 60 levels)
+levels() {
+    (( "$#" >= 1 && "$#" <= 60 )) ||
+        error "Invalid number of parameters. (given $#, expected [1, 60])"
+    for arg in "$@"
+    do
+        (( "$arg" >= 1 && "$arg" <= 60 )) ||
+            error "Invalid parameter. (given $arg, expected [1, 60])"
+    done
+    # if requesting all 60 levels, don't add to query because getting all
+    # levels is default behavior
+    # echo "$(cat -)" causes a shellcheck error in this case so
+    # use cat <&0 to redirect stdin to stdout instead
+    (( "$#" == 60 )) && cat <&0 && return 0
+    # replace spaces with commas
+    values=$(tr -s '[:blank:]' ',' <<< "$*")
+    echo "$(cat -)&${FUNCNAME[0]}=$values"
+}
+
+# Return objects with a matching value in the hidden attribute
+# $1: true | false
+hidden() {
+    (( "$#" == 1 )) ||
+        error "Invalid number of parameters. (given $#, expected 1)"
+    [[ "$1" == "true" || "$1" == "false" ]] ||
+        error "Invalid parameter. Must be either 'true' or 'false' but got: $*"
+    echo "$(cat -)&${FUNCNAME[0]}=$1"
+}
+
+error() {
+    >&2 echo -e "From function: ${FUNCNAME[1]}\n$1"
+    exit 1
+}
+
+url="$(wanikani::subjects | types kanji vocabulary | hidden false | levels {1..2})"
+echo "\"$url\"" "${HEADERS[@]}"
